0c91b638b2db4201c059f90d7b73fa683c5540b6
LUFT-AQUILA <mail@luftaquila.io>
2024-01-27 21:59:23 +0900

    add: rdev for event capture

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
 use display_info::DisplayInfo;
-use enigo::*;
+use rdev::*;
 
 fn main() {
-  let display_infos = DisplayInfo::all().unwrap();
-  for display_info in display_infos {
-    println!("display_info {display_info:?}");
-  }
+    let display_infos = DisplayInfo::all().unwrap();
+    for display_info in display_infos {
+        println!("display_info {display_info:?}");
+    }
 
-  while true {
-      let mut enigo = Enigo::new();
-      let cursor_location: (i32, i32) = enigo.mouse_location();
-      println!("cursor_location {cursor_location:?}");
-  }
+    loop {
+        if let Err(error) = listen(callback) {
+            println!("Error: {:?}", error)
+        }
+    }
+
+}
+
+
+fn callback(event: Event) {
+    match event.event_type {
+        EventType::KeyPress(key) => {
+            println!("kepress: {:?}", event);
+        },
+
+        EventType::KeyRelease(key) => {
+            println!("keyrelease: {:?}", event);
+        },
+
+        EventType::ButtonPress(button) => {
+            println!("buttonpress: {:?}", event);
+        },
+
+        EventType::ButtonRelease(button) => {
+            println!("buttonrelease: {:?}", event);
+        },
+
+        EventType::MouseMove { x, y } => {
+            println!("mousemove: {:?}", event);
+        },
+
+        EventType::Wheel { delta_x, delta_y } => {
+            println!("wheel: {:?}", event);
+        }
+    }
+
+    // println!("My callback {:?}", event);
+    // match event.name {
+    //     Some(string) => println!("User wrote {:?}", string),
+    //     None => (),
+    // }
 }
