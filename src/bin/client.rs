d01b38f4ac2a5fa4ce818103e5e74faeb05458e6
오병준 <mail@luftaquila.io>
2024-03-17 23:58:15 +0900

    add: working Event exchange

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,69 +1,61 @@
-use std::net::UdpSocket;
-use display_info::DisplayInfo;
 use rdev::*;
+use std::env;
+use std::io::Read;
+use std::net::TcpStream;
 
-const server_ip: &str = "127.0.0.1";
-const port: u16 = 15007;
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-fn main() -> std::io::Result<()> {
-    let display = DisplayInfo::all().unwrap();
-
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-
-    let hi = "hello";
-    let result = socket.send_to(hi.as_bytes(), (server_ip, port));
-
-    println!("res: {:?}", result);
-
-    loop {
-        // socket.recv_from
-
+    if args.len() < 2 {
+        eprintln!("[ERR]: no server address specified");
+        return;
     }
 
-    Ok(())
+    let server = &args[1];
 
-    // loop {
-    //     if let Err(error) = listen(callback) {
-    //         println!("Error: {:?}", error)
-    //     }
-    // }
+    let mut stream = match TcpStream::connect(server) {
+        Ok(stream) => stream,
+        Err(e) => {
+            eprintln!("[ERR] server {} connection failed: {}", server, e);
+            return;
+        }
+    };
+
+    println!("[INF] server connected!");
+
+    let mut buffer = Vec::with_capacity(50);
+    let mut size = [0u8; 8];
+
+    loop {
+        match stream.read_exact(&mut size) {
+            Ok(_) => {},
+            Err(e) => {
+                eprintln!("[ERR] event size read failed: {}", e);
+                continue;
+            }
+        };
+
+        let len = u64::from_be_bytes(size) as usize;
+        buffer.resize(len, 0);
+
+        match stream.read_exact(&mut buffer[..len]) {
+            Ok(_) => {
+                let event: Event = match bincode::deserialize(&buffer) {
+                    Ok(event) => event,
+                    Err(e) => {
+                        eprintln!("[ERR] event deserialization failed: {}", e);
+                        continue;
+                    }
+                };
+
+                println!("[EVT] <{}> {:?}", len, event);
+                buffer.clear();
+            }
+            Err(e) => {
+                eprintln!("[ERR] stream read failed: {}", e);
+                break;
+            }
+        }
+    }
 }
 
-fn config() {
-
-}
-
-
-// fn callback(event: Event) {
-//     match event.event_type {
-//         EventType::KeyPress(key) => {
-//             println!("kepress: {:?}", event);
-//         },
-
-//         EventType::KeyRelease(key) => {
-//             println!("keyrelease: {:?}", event);
-//         },
-
-//         EventType::ButtonPress(button) => {
-//             println!("buttonpress: {:?}", event);
-//         },
-
-//         EventType::ButtonRelease(button) => {
-//             println!("buttonrelease: {:?}", event);
-//         },
-
-//         EventType::MouseMove { x, y } => {
-//             println!("mousemove: {:?}", event);
-//         },
-
-//         EventType::Wheel { delta_x, delta_y } => {
-//             println!("wheel: {:?}", event);
-//         }
-//     }
-
-//     // println!("My callback {:?}", event);
-//     // match event.name {
-//     //     Some(string) => println!("User wrote {:?}", string),
-//     //     None => (),
-//     // }
-// }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,48 +1,39 @@
-use std::mem;
-use std::net::{UdpSocket, SocketAddr};
-use display_info::DisplayInfo;
 use rdev::*;
+use std::io::Write;
+use std::net::TcpListener;
+use display_info::DisplayInfo;
 
-const port: u16 = 15007;
+const PORT: u16 = 2426;
 
 fn main() {
-    let display = DisplayInfo::all().unwrap();
+    let _display = DisplayInfo::all().unwrap();
 
-    let socket = UdpSocket::bind(("127.0.0.1", port)).expect("bind failed!");
+    let listener = TcpListener::bind(("localhost", PORT))
+        .expect(&format!("[ERR] port {} bind failed!", PORT));
 
-    // println!("waiting for client...");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("[INF] inbound connection: {}", stream.peer_addr().unwrap());
 
-    // let mut buf: [u8; 5] = [0; 5];
-    // let (nb, src) = socket.recv_from(&mut buf).expect("handshake failed!");
+                if let Err(error) = listen(move |event| {
+                    println!("[EVT] {:?}", event);
 
-    println!("start monitoring..");
+                    let encoded = bincode::serialize(&event).unwrap();
+                    let size = encoded.len().to_be_bytes();
 
-    // monitor(socket, src);
-    monitor(socket);
+                    if let Err(e) = stream.write_all(&size) {
+                        eprintln!("[ERR] TCP stream write failed: {}", e);
+                    } else if let Err(e) = stream.write_all(&encoded) {
+                        eprintln!("[ERR] TCP stream write failed: {}", e);
+                    }
+                }) {
+                    eprintln!("[ERR] input capture error: {:?}", error);
+                }
+            }
+            Err(e) => {
+                eprintln!("[ERR] TCP connection failed: {}", e);
+            }
+        }
+    }
 }
-
-fn monitor(socket: UdpSocket/*, src: SocketAddr*/) {
-    // loop {
-        let res = listen(move |event| {
-            println!("event: {:?}, socket: {:?}", event, socket);
-        });
-        // println!("res: {:?}", res);
-        // if let Err(error) = listen(callback) {
-        //     println!("Error: {:?}", error)
-        // }
-    // }
-}
-
-fn callback(event: Event) {
-    println!("event: {:?}", event);
-    // let bytes: &[u8; mem::size_of::<Event>()];
-    // let (nb, src) = socket.send_to(&mut buf).expect("config failed");
-}
-
-// fn config(socket: &UdpSocket, display: Vec<DisplayInfo>) -> Conf {
-//     let mut buf = Conf { host_origin: Point { x: 10, y: 20 }, client_origin: Point { x: 30, y: 40 } };
-//     let bytes: &[u8; mem::size_of::<Conf>()];
-//     let (nb, src) = socket.recv_from(&mut buf).expect("config failed");
-
-//     buf
-// }
